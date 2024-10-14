1b70887a69fb819b4a717872bc6af8ef88f6aa3a
wilson <wilsonbolledula@gmail.com>
2024-10-14 18:47:33 +0530

    after modification

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('Hello, Docker!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server is running on port 3000');
 });
